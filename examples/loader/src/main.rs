9e84720f6c3522235385fa44f0a2ef6716beba5a
raymon <guoraymon@gmail.com>
2024-12-05 20:48:19 +0800

    exercise 1

--- examples/loader/src/main.rs
+++ examples/loader/src/main.rs
@@ -5,15 +5,18 @@
 use axstd::println;
 
 const PLASH_START: usize = 0xffff_ffc0_2200_0000;
+const HEADER_SIZE: usize = 4;
 
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
-    let apps_start = PLASH_START as *const u8;
-    let apps_size = 32; // Dangerous!!! We need to get accurate size of apps.
+    let image_start = PLASH_START as *const u8;
+    let header = unsafe { core::slice::from_raw_parts(image_start, HEADER_SIZE) };
+    let apps_size = u32::from_be_bytes([header[0], header[1], header[2], header[3]]);
 
     println!("Load payload ...");
 
-    let code = unsafe { core::slice::from_raw_parts(apps_start, apps_size) };
+    let apps_start = unsafe { image_start.add(HEADER_SIZE) };
+    let code = unsafe { core::slice::from_raw_parts(apps_start, apps_size as usize) };
     println!("content: {:?}: ", code);
 
     println!("Load payload ok!");
